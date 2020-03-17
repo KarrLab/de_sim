04b5e659e8d3512dc8f5c8e3501b18b6f078703b
Arthur Goldberg <artgoldberg@gmail.com>
2020-03-17 11:49:35 -0400

    add getter and setter properties

--- de_sim/simulation_config.py
+++ de_sim/simulation_config.py
@@ -27,23 +27,23 @@ class SimulationConfig:
     - Metadata directory
 
     Attributes:
-        time_max (:obj:`float`): maximum simulation time
-        time_init (:obj:`float`, optional): initial simulation time
-        random_seed (:obj:`int`, optional): random number generator seed
-        stop_condition (:obj:`function`, optional): if provided, a function that takes one argument,
+        _time_max (:obj:`float`): maximum simulation time
+        _time_init (:obj:`float`, optional): initial simulation time
+        _random_seed (:obj:`int`, optional): random number generator seed
+        _stop_condition (:obj:`function`, optional): if provided, a function that takes one argument,
             the simulation time; a simulation terminates if the function returns `True`
-        progress (:obj:`bool`, optional): if `True`, output a bar that dynamically reports the
+        _progress (:obj:`bool`, optional): if `True`, output a bar that dynamically reports the
             simulation's progress
-        metadata_dir (:obj:`str`, optional): directory for saving metadata; if not provided,
+        _metadata_dir (:obj:`str`, optional): directory for saving metadata; if not provided,
             then metatdata should be saved before another simulation is run with the same `SimulationEngine`
     """
 
-    time_max: float
-    time_init: float = 0.0
-    random_seed: int = None
-    stop_condition: object = None   # the type is 'function', but dataclass rejects it; check it below
-    progress: bool = False
-    metadata_dir: str = None
+    _time_max: float
+    _time_init: float = 0.0
+    _random_seed: int = None
+    _stop_condition: object = None   # the type is 'function', but dataclass rejects it; check it below
+    _progress: bool = False
+    _metadata_dir: str = None
 
     def validate(self):
         # validate types
@@ -59,13 +59,68 @@ class SimulationConfig:
                         raise SimulatorError(f"{field.name} ('{attr}') is not a(n) {field.type.__name__}")
 
         # other validation
-        if self.time_max <= self.time_init:
-            raise SimulatorError(f'time_max ({self.time_max}) must be greater than time_init ({self.time_init})')
+        if self._time_max <= self._time_init:
+            raise SimulatorError(f'time_max ({self._time_max}) must be greater than time_init ({self._time_init})')
 
         # make sure stop_condition is a function
-        if self.stop_condition is not None and not isinstance(self.stop_condition, types.FunctionType):
-            raise SimulatorError(f"stop_condition ('{self.stop_condition}') is not a function")
+        if self._stop_condition is not None and not isinstance(self._stop_condition, types.FunctionType):
+            raise SimulatorError(f"stop_condition ('{self._stop_condition}') is not a function")
 
         # ensure that metadata_dir is a directory
-        if self.metadata_dir is not None and not Path(self.metadata_dir).is_dir():
-            raise SimulatorError(f"metadata_dir ('{self.metadata_dir}') is not a directory")
+        if self._metadata_dir is not None and not Path(self._metadata_dir).is_dir():
+            raise SimulatorError(f"metadata_dir ('{self._metadata_dir}') is not a directory")
+
+    # getters and setters for all attributes
+    @property
+    def time_max(self):
+        return self._time_max
+
+    @time_max.setter
+    def time_max(self, value):
+        self._time_max = value
+        self.validate()
+
+    @property
+    def time_init(self):
+        return self._time_init
+
+    @time_init.setter
+    def time_init(self, value):
+        self._time_init = value
+        self.validate()
+
+    @property
+    def random_seed(self):
+        return self._random_seed
+
+    @random_seed.setter
+    def random_seed(self, value):
+        self._random_seed = value
+        self.validate()
+
+    @property
+    def stop_condition(self):
+        return self._stop_condition
+
+    @stop_condition.setter
+    def stop_condition(self, value):
+        self._stop_condition = value
+        self.validate()
+
+    @property
+    def progress(self):
+        return self._progress
+
+    @progress.setter
+    def progress(self, value):
+        self._progress = value
+        self.validate()
+
+    @property
+    def metadata_dir(self):
+        return self._metadata_dir
+
+    @metadata_dir.setter
+    def metadata_dir(self, value):
+        self._metadata_dir = value
+        self.validate()
--- tests/test_simulation_config.py
+++ tests/test_simulation_config.py
@@ -41,15 +41,14 @@ class TestSimulationConfig(unittest.TestCase):
 
         # check defaults
         simulation_config = SimulationConfig(self.time_max)
-        self.assertEquals(simulation_config.time_max, self.time_max)
         self.assertEquals(simulation_config.time_init, 0.0)
         self.assertEquals(simulation_config.random_seed, None)
         self.assertEquals(simulation_config.stop_condition, None)
         self.assertEquals(simulation_config.progress, False)
         self.assertEquals(simulation_config.metadata_dir, None)
 
-        # check param names
-        simulation_config = SimulationConfig(time_max=self.time_max)
+        # check keywords
+        simulation_config = SimulationConfig(_time_max=self.time_max)
         self.assertEquals(simulation_config.time_max, self.time_max)
 
     def test_validate(self):
@@ -58,7 +57,7 @@ class TestSimulationConfig(unittest.TestCase):
         except SimulatorError:
             self.fail("simulation_config.validate() shouldn't raise SimulatorError")
 
-        # validate with random_seed == None
+        # validate with defaults
         simulation_config = SimulationConfig(self.time_max)
         try:
             simulation_config.validate()
@@ -70,7 +69,7 @@ class TestSimulationConfig(unittest.TestCase):
         with self.assertRaisesRegex(SimulatorError, 'time_max .* is not a\(n\) float'):
             simulation_config.validate()
 
-        simulation_config = SimulationConfig(self.time_max, time_init=set())
+        simulation_config = SimulationConfig(self.time_max, _time_init=set())
         with self.assertRaisesRegex(SimulatorError, 'time_init .* is not a\(n\) float'):
             simulation_config.validate()
 
@@ -78,10 +77,41 @@ class TestSimulationConfig(unittest.TestCase):
         with self.assertRaisesRegex(SimulatorError, 'time_max .* must be greater than time_init .*'):
             simulation_config.validate()
 
-        simulation_config = SimulationConfig(self.time_max, stop_condition='hi')
+        simulation_config = SimulationConfig(self.time_max, _stop_condition='hi')
         with self.assertRaisesRegex(SimulatorError, "stop_condition .* is not a function"):
             simulation_config.validate()
 
-        simulation_config = SimulationConfig(self.time_max, metadata_dir='not a dir')
+        simulation_config = SimulationConfig(self.time_max, _metadata_dir='not a dir')
         with self.assertRaisesRegex(SimulatorError, "metadata_dir .* is not a directory"):
             simulation_config.validate()
+
+    def test_properties(self):
+        # test all setters and getters
+        new_time_max = self.time_max + 0.1
+        self.simulation_config.time_max = new_time_max
+        self.assertEquals(self.simulation_config.time_max, new_time_max)
+
+        new_time_init = self.time_init + 0.1
+        self.simulation_config.time_init = new_time_init
+        self.assertEquals(self.simulation_config.time_init, new_time_init)
+
+        new_random_seed = self.random_seed + 1
+        self.simulation_config.random_seed = new_random_seed
+        self.assertEquals(self.simulation_config.random_seed, new_random_seed)
+
+        def g(): pass
+        new_stop_condition = g
+        self.simulation_config.stop_condition = new_stop_condition
+        self.assertEquals(self.simulation_config.stop_condition, new_stop_condition)
+
+        new_progress = not self.progress
+        self.simulation_config.progress = new_progress
+        self.assertEquals(self.simulation_config.progress, new_progress)
+
+        new_metadata_dir = tempfile.mkdtemp(dir=self.tmp_dir)
+        self.simulation_config.metadata_dir = new_metadata_dir
+        self.assertEquals(self.simulation_config.metadata_dir, new_metadata_dir)
+
+        # test validation in setter
+        with self.assertRaisesRegex(SimulatorError, 'time_max .* must be greater than time_init .*'):
+            self.simulation_config.time_max = self.time_init - 1
